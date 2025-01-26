4f8842da31d835c84f691646822fc6d2ec39e929
script-money <hello@script.money>
2025-01-26 14:08:06 +0800

    :sparkles: Reply with note tweet

--- apidance/exceptions.py
+++ apidance/exceptions.py
@@ -1,28 +1,38 @@
 class AuthenticationError(Exception):
-    "Raised when authentication fails"
-    pass
+    """Raised when authentication fails."""
 
 
 class RateLimitError(Exception):
-    "Raised when API rate limit is exceeded"
-    pass
+    """Raised when rate limit is exceeded."""
+
+
+class ValidationError(Exception):
+    """Raised when request validation fails."""
+
+
+class TweetNotFoundError(Exception):
+    """Raised when tweet is not found."""
+
+
+class UserNotFoundError(Exception):
+    """Raised when user is not found."""
 
 
 class InsufficientCreditsError(Exception):
     "Raised when the account has insufficient API credits"
-    pass
+
+
+class PremiumRequiredError(Exception):
+    "Upgrade to premium to use this feature"
 
 
 class TimeoutError(Exception):
     "Raised when a request times out"
-    pass
 
 
 class InvalidUserIdError(Exception):
     "Raised when user ID format is invalid"
-    pass
 
 
 class AccountSuspendedError(Exception):
     "Raised when the account is suspended"
-    pass
